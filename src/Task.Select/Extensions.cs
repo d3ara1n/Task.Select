d85bd656bb54bcce340e10c2dd6fd42d5fe23124
Dearain <d3ara1n@outlook.com>
2025-09-17 10:12:06 +0800

    添加项目文件。

---
+++ src/Task.Select/Extensions.cs
@@ -0,0 +1,51 @@
+﻿namespace Task.Select
+{
+    public static class Extensions
+    {
+        extension(System.Threading.Tasks.Task)
+        {
+            public static async Task<(T1, T2)> Select<T1, T2>(Task<T1> t1, Task<T2> t2)
+            {
+                await System.Threading.Tasks.Task.WhenAll(t1, t2);
+                return (t1.Result, t2.Result);
+            }
+
+            public static async Task<(T1, T2, T3)> Select<T1, T2, T3>(Task<T1> t1, Task<T2> t2, Task<T3> t3)
+            {
+                await System.Threading.Tasks.Task.WhenAll(t1, t2, t3);
+                return (t1.Result, t2.Result, t3.Result);
+            }
+
+            public static async Task<(T1, T2, T3, T4)> Select<T1, T2, T3, T4>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4)
+            {
+                await System.Threading.Tasks.Task.WhenAll(t1, t2, t3, t4);
+                return (t1.Result, t2.Result, t3.Result, t4.Result);
+            }
+
+            public static async Task<(T1, T2, T3, T4, T5)> Select<T1, T2, T3, T4, T5>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5)
+            {
+                await System.Threading.Tasks.Task.WhenAll(t1, t2, t3, t4, t5);
+                return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result);
+            }
+
+            public static async Task<(T1, T2, T3, T4, T5, T6)> Select<T1, T2, T3, T4, T5, T6>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6)
+            {
+                await System.Threading.Tasks.Task.WhenAll(t1, t2, t3, t4, t5, t6);
+                return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result);
+            }
+
+
+            public static async Task<(T1, T2, T3, T4, T5, T6, T7)> Select<T1, T2, T3, T4, T5, T6, T7>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6, Task<T7> t7)
+            {
+                await System.Threading.Tasks.Task.WhenAll(t1, t2, t3, t4, t5, t6, t7);
+                return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result, t7.Result);
+            }
+
+            public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8)> Select<T1, T2, T3, T4, T5, T6, T7, T8>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6, Task<T7> t7, Task<T8> t8)
+            {
+                await System.Threading.Tasks.Task.WhenAll(t1, t2, t3, t4, t5, t6, t7, t8);
+                return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result, t7.Result, t8.Result);
+            }
+        }
+    }
+}
