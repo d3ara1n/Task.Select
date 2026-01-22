0bea62ca15865ee04e09ad8ab8fa49bf982724b8
Dearain <d3ara1n@outlook.com>
2026-01-22 20:57:58 +0800

    fix: fix result

--- src/TaskSelect/Extensions.cs
+++ src/TaskSelect/Extensions.cs
@@ -4,47 +4,40 @@
     {
         extension(System.Threading.Tasks.Task)
         {
-            public static async Task<(T1, T2)> Select<T1, T2>(Task<T1> t1, Task<T2> t2)
+            public static async Task Select<T1, T2>(Task<T1> t1, Task<T2> t2)
             {
                 await Task.WhenAny(t1, t2);
-                return (t1.Result, t2.Result);
             }
 
-            public static async Task<(T1, T2, T3)> Select<T1, T2, T3>(Task<T1> t1, Task<T2> t2, Task<T3> t3)
+            public static async Task Select<T1, T2, T3>(Task<T1> t1, Task<T2> t2, Task<T3> t3)
             {
                 await Task.WhenAny(t1, t2, t3);
-                return (t1.Result, t2.Result, t3.Result);
             }
 
-            public static async Task<(T1, T2, T3, T4)> Select<T1, T2, T3, T4>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4)
+            public static async Task Select<T1, T2, T3, T4>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4)
             {
                 await Task.WhenAny(t1, t2, t3, t4);
-                return (t1.Result, t2.Result, t3.Result, t4.Result);
             }
 
-            public static async Task<(T1, T2, T3, T4, T5)> Select<T1, T2, T3, T4, T5>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5)
+            public static async Task Select<T1, T2, T3, T4, T5>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5)
             {
                 await Task.WhenAny(t1, t2, t3, t4, t5);
-                return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result);
             }
 
-            public static async Task<(T1, T2, T3, T4, T5, T6)> Select<T1, T2, T3, T4, T5, T6>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6)
+            public static async Task Select<T1, T2, T3, T4, T5, T6>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6)
             {
                 await Task.WhenAny(t1, t2, t3, t4, t5, t6);
-                return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result);
             }
 
 
-            public static async Task<(T1, T2, T3, T4, T5, T6, T7)> Select<T1, T2, T3, T4, T5, T6, T7>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6, Task<T7> t7)
+            public static async Task Select<T1, T2, T3, T4, T5, T6, T7>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6, Task<T7> t7)
             {
                 await Task.WhenAny(t1, t2, t3, t4, t5, t6, t7);
-                return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result, t7.Result);
             }
 
-            public static async Task<(T1, T2, T3, T4, T5, T6, T7, T8)> Select<T1, T2, T3, T4, T5, T6, T7, T8>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6, Task<T7> t7, Task<T8> t8)
+            public static async Task Select<T1, T2, T3, T4, T5, T6, T7, T8>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6, Task<T7> t7, Task<T8> t8)
             {
                 await Task.WhenAny(t1, t2, t3, t4, t5, t6, t7, t8);
-                return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result, t7.Result, t8.Result);
             }
         }
     }
